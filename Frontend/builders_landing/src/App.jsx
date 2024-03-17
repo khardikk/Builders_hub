f6925a8f27deb26c7e966fed0a29b68d362bfc18
hardikk kamboj <kambojpushkal@gmail.com>
2024-03-17 16:41:34 +0530

    Project Setup

---
+++ Frontend/builders_landing/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Landing page using tailwind and React
+    </h1>
+    </>
+  )
+}
+
+export default App
