6fec3aa2e5ce5a109430a79c93f152dd0d34f468
hardikk kamboj <kambojpushkal@gmail.com>
2024-03-19 19:02:43 +0530

    Gradient added to Heading

--- Frontend/builders_landing/src/components/Header.jsx
+++ Frontend/builders_landing/src/components/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
     <div className='flex justify-center items-center h-screen'>
       <div className='max-w-5xl flex flex-col items-center'>
         <img src={headergraphic} alt="builders" className="w-60 h-86 object-cover my-12"/>
-    <h1 className="text-8xl font-bold font-Inter leading-none">
+    <h1 className=" bg-gradient-to-b from-white via-slate-200 to-slate-400 inline-block text-transparent bg-clip-text text-8xl font-bold font-Inter leading-none">
     We Build Stuff<br></br>
     We are Builders Hub.
     </h1>
-    <p className='text-3xl font-light leading-normal  my-5'>We are a community of builders and we believe in building projects that we do not leave in between. 
+    <p className='text-3xl font-light leading-normal  my-5 text-gray-300'>We are a community of builders and we believe in building projects that we do not leave in between. 
       We collaborate to make the building experience better, faster and more enjoyable.</p></div>
     </div>
     
