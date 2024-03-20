59471fd8c3a701e244c9fc27957d0676bb078d17
hardikk kamboj <kambojpushkal@gmail.com>
2024-03-20 21:47:15 +0530

    Update Navbar buttons styling

--- Frontend/builders_landing/src/components/Navbar.jsx
+++ Frontend/builders_landing/src/components/Navbar.jsx
@@ -4,9 +4,9 @@ const Navbar = () => {
   return (
     <div className='w-full flex '>
     <div className='text-base font-semibold items-center flex-1 '>
-    <button className=' mx-6 py-2 my-2 bg-zinc-900 px-3.5 rounded-lg border-stone-500 ring-1 ring-gray-400'>Work</button>
+    <button className=' mx-6 py-2 my-2 bg-zinc-900 px-3.5 rounded-lg border-stone-500 ring-1 ring-gray-400 hover:bg-zinc-100 hover:text-black'>Work</button>
     <button className='mx-6'>About</button>
-    <button className='mx-6'>Contact</button>
+    <button className='mx-6 bg-gradient-to-r from-amber-300 via-cyan-400 to-indigo-400 inline-block text-transparent bg-clip-text'>Contact</button>
     </div>
     </div>
   )
