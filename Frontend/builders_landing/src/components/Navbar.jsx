0e0706f5da5ac9e5ac4d7ec54584ec58e15aa32f
hardikk kamboj <kambojpushkal@gmail.com>
2024-03-20 21:38:25 +0530

    Border color navbar

--- Frontend/builders_landing/src/components/Navbar.jsx
+++ Frontend/builders_landing/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='w-full flex '>
     <div className='text-base font-semibold items-center flex-1 '>
-    <button className=' mx-6 py-2 my-2 bg-zinc-900 px-3.5 rounded-lg border-stone-500 ring-1 ring-gray-600'>Work</button>
+    <button className=' mx-6 py-2 my-2 bg-zinc-900 px-3.5 rounded-lg border-stone-500 ring-1 ring-gray-400'>Work</button>
     <button className='mx-6'>About</button>
     <button className='mx-6'>Contact</button>
     </div>
