15631e0fbf8da390186b352444ff6f63a33b54dc
hardikk kamboj <kambojpushkal@gmail.com>
2024-03-21 23:54:03 +0530

    Our builds secttion start

--- Frontend/builders_landing/src/components/About.jsx
+++ Frontend/builders_landing/src/components/About.jsx
@@ -4,9 +4,9 @@ import aboutus from '../assets/aboutus.png'
 const About = () => {
   return (
     <>
-    <div class="flex flex-row ml-[5rem] justify-between mt-8">
+    <div class="flex flex-row justify-between mt-8">
     <div class="flex flex-col">
-        <h1 class="text-left mb-10 font-Inter font-bold text-5xl">About Us</h1>
+        <h1 className="text-left mb-10 font-Inter font-bold text-5xl">About Us</h1>
         <p class="text-left w-[36rem]  font-DMSans text-2xl leading-relaxed ">With over 4 years of experience in digital product design, I'm passionate about creating engaging and functional solutions that meet users' needs. My focus on user research and usability ensures that 
             each design not only looks great but also delivers an exceptional experience</p>    
     </div>
--- Frontend/builders_landing/src/components/OurBuilds.jsx
+++ Frontend/builders_landing/src/components/OurBuilds.jsx
@@ -4,8 +4,8 @@ import Card from './Card'
 const OurBuilds = () => {
   return (
     <div>
-        <h1> Out Builds</h1>
-        <p>Here's a glimpse of some of my recent and exciting projects.
+        <h1 className="text-left mb-5 font-Inter font-bold text-5xl"> Our Builds</h1>
+        <p className=" w-[48rem] text-left mb-10 font-Inter font-normal text-lg text-neutral-400">Here's a glimpse of some of my recent and exciting projects.
          Each of them reflects my focus on user-centered design and my commitment to excellence in user experience.</p>
         <Card />
         <Card />
