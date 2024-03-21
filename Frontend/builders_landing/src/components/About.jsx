4ea306d4d1c6805125f9c1c27c74c579bd765ccb
hardikk kamboj <kambojpushkal@gmail.com>
2024-03-21 23:40:46 +0530

    About us Css

--- Frontend/builders_landing/src/components/About.jsx
+++ Frontend/builders_landing/src/components/About.jsx
@@ -6,8 +6,8 @@ const About = () => {
     <>
     <div class="flex flex-row ml-[5rem] justify-between mt-8">
     <div class="flex flex-col">
-        <h1 class="text-left mb-4  ">About Us</h1>
-        <p class="text-left leading-7 w-[36rem]">With over 4 years of experience in digital product design, I'm passionate about creating engaging and functional solutions that meet users' needs. My focus on user research and usability ensures that 
+        <h1 class="text-left mb-10 font-Inter font-bold text-5xl">About Us</h1>
+        <p class="text-left w-[36rem]  font-DMSans text-2xl leading-relaxed ">With over 4 years of experience in digital product design, I'm passionate about creating engaging and functional solutions that meet users' needs. My focus on user research and usability ensures that 
             each design not only looks great but also delivers an exceptional experience</p>    
     </div>
     <div class="flex mr-[5rem]">
