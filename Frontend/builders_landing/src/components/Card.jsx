15965665c45ab35dbcca46c8c3c6346506b9d735
hardikk kamboj <kambojpushkal@gmail.com>
2024-03-22 01:11:34 +0530

    Refactored Card component and updated project description

--- Frontend/builders_landing/src/components/Card.jsx
+++ Frontend/builders_landing/src/components/Card.jsx
@@ -8,13 +8,12 @@ const Card = () => {
       <div className='py-16 px-16'>
       <div className='grid grid-cols-3 h-full'>
         {/* Text div */}
-        <div className='pr-16  flex flex-col'>
+        <div className='pr-4  flex flex-col'>
         <div className='h-full items-start col-span-1 flex flex-col justify-around'>
           <h1 className='mb-6 font-Inter font-bold text-4xl text-left'>Cashflow</h1>
           <span className='py-6 font-Inter font-normal text-base text-left'>2024-Fullstack</span>
           <p className='font-Inter font-normal text-lg text-left'>
-            It is a web application that helps you to track your expenses and income. It is a
-            full-stack application that is built using React, Node.js, Express, and MongoDB.
+            Transformed an e-commerce website with a modern redesign, optimizing the shopping journey and elevating brand identity. Resulted in improved user engagement, increased sales, and a seamless shopping experience.
           </p>
           <img className='w-[40px] h-[24px] mb-20 mt-14' src={stack} alt='stack' />
           <button className='ring-2 ring-neutral-300 rounded-lg py-3 px-6 font-Inter font-semibold text-base'>
