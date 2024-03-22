22554cdb10a1019d083d17962997be2228a8d617
hardikk kamboj <kambojpushkal@gmail.com>
2024-03-23 00:33:37 +0530

    Update font size in Skills component

--- Frontend/builders_landing/src/components/Skills.jsx
+++ Frontend/builders_landing/src/components/Skills.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Skills = () => {
   return (
     <div data-aos="fade-up" data-aos-duration="1500" >
-        <h1 class="text-left mb-[2rem] font-Inter font-bold text-4xl ">
+        <h1 class="text-left mb-[2rem] font-Inter font-bold text-3xl ">
         Stuff we build on
         </h1>
         
         <ul class="marker:text-violet-500 list-outside list-disc ml-4">
-          <div class="grid grid-cols-4 text-left ">
+          <div class="grid grid-cols-4 text-left font-Inter font-normal text-lg text-zinc-400 ">
         <li class="mb-[1rem]">HTML</li>
         <li>CSS</li>
         <li>JavaScript</li>
