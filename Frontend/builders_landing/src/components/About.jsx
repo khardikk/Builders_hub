b2507dcaf0510ce49d2c2d9f80383ab00110dd74
hardikk kamboj <kambojpushkal@gmail.com>
2024-03-23 00:54:56 +0530

    Moved Social Icons

--- Frontend/builders_landing/src/components/About.jsx
+++ Frontend/builders_landing/src/components/About.jsx
@@ -4,7 +4,7 @@ import connect from '../assets/connect.png';
 const About = () => {
   return (
     <>
-    <div data-aos="fade-up" data-aos-duration="1500" id='about-section' class="flex flex-row justify-between mt-8 ">
+    <div data-aos="fade-up" data-aos-duration="1500" id='about-section' class="flex flex-row justify-between mt-8 mb-20">
     <div class="flex flex-col w-[51.375rem]">
         <h1 className="text-left mb-10 font-Inter font-bold text-5xl">About Us</h1>
         <p class="text-left w-[45rem] font-DMSans text-2xl leading-relaxed mb-8">With over 4 years of experience in digital product design, I'm passionate about creating engaging and functional solutions that meet users' needs. My focus on user research and usability ensures that 
--- Frontend/builders_landing/src/components/Contact.jsx
+++ Frontend/builders_landing/src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
           <div className='flex flex-col items-center justify-center my-20 w-[602px]'>
             <div style={{ marginBottom: '20px' }}>
               <h1 className='font-Inter font-bold text-5xl mt-6 mb-6'> Let's Talk</h1>
-              <p className='font-Inter font-normal text-lg text-zinc-400 w-full'>Interested in working together or have a question? Feel free to reach out. We  Believe in collaboration and in helping out fellow builders.
+              <p className='font-Inter font-normal text-lg text-zinc-400 w-full'>Interested in working together or have a question? Feel free to reach out to any of us on our <b>Social Links</b> provided above. We  Believe in collaboration and in helping out fellow builders.
                 Let’s turn your ideas into amazing realities. Builders keep Building.</p>
             </div>
 
