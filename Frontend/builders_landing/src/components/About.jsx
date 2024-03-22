88f0d3297f5d25d33366839927d280c936d6af89
akshatg5 <akshatgirdhar5@gmail.com>
2024-03-23 00:56:57 +0530

    edits

--- Frontend/builders_landing/src/components/About.jsx
+++ Frontend/builders_landing/src/components/About.jsx
@@ -7,8 +7,7 @@ const About = () => {
     <div data-aos="fade-up" data-aos-duration="1500" id='about-section' class="flex flex-row justify-between mt-8 mb-20">
     <div class="flex flex-col">
         <h1 className="text-left mb-10  font-Inter font-bold text-5xl">About Us</h1>
-        <p class="text-left w-[36rem]  font-DMSans text-2xl leading-relaxed ">With over 4 years of experience in digital product design, I'm passionate about creating engaging and functional solutions that meet users' needs. My focus on user research and usability ensures that 
-            each design not only looks great but also delivers an exceptional experience</p>    
+        <p class="text-left w-[36rem]  font-DMSans text-2xl leading-relaxed ">With over 4 years of experience in digital product design, I'm passionate about creating engaging and functional solutions that meet users' needs. My focus on user research and usability ensures that each design not only looks great but also delivers an exceptional experience</p>    
     </div>
     <div class="flex mr-[5rem]">
         <img src={aboutus}></img>
