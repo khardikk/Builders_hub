ce14b0be2cf2fd0d590f5dac5d187974661c2986
akshatg5 <akshatgirdhar5@gmail.com>
2024-03-27 01:23:46 +0530

    Logos rendering

--- Frontend/builders_landing/src/components/Card.jsx
+++ Frontend/builders_landing/src/components/Card.jsx
@@ -1,41 +1,67 @@
-import React from 'react';
-import Tailwind from '../assets/Tailwind.png';
-import rctlogo from '../assets/React-logo.png';
-import figmalogo from '../assets/Figma-logo.png';
+import React from "react";
+import tailwindlogo from "../assets/Tailwind.png";
+import rctlogo from "../assets/React-logo.png";
+import figmalogo from "../assets/Figma-logo.png";
+import nodejslogo from "../assets/nodejs-logo.png";
+import expresslogo from "../assets/express-logo.png";
+import mongodblogo from "../assets/mongodb-logo.png";
+import webflowlogo from "../assets/webflow-logo.png";
+import sanitylogo from "../assets/sanity-logo.png";
 
 const Card = (props) => {
-  const { src,projects,ProjectName,Projectstack,ProjectDesc} = props;
+  const { src, projects, ProjectName, Projectstack, ProjectDesc, logos } =
+    props;
   return (
-    <div data-aos="fade-right" data-aos-duration="1500" className='bg-neutral-900 rounded-3xl ring-1 ring-neutral-500 w-1200 h-620 my-10'>
-      <div className='py-16 px-16 max-[480px]:py-8 max-[480px]:px-8'>
-      <div className='grid grid-cols-3 h-full max-[480px]:flex max-[480px]:flex-col'>
-        {/* Text div */}
-        <div className='pr-4  flex flex-col'>
-        <div className='h-full items-start col-span-1 flex flex-col justify-around max-sm:mb-6'>
-          <h1 className='mb-6 font-Inter font-bold text-4xl text-left max-sm:w-[16rem] max-sm:text-left'>{ProjectName}</h1>
-          <span className='py-6 font-Inter font-normal text-base text-left max-sm:text-left max-sm:w-[15rem]'>{Projectstack}</span>
-          <p className='font-Inter font-normal text-lg text-left max-sm:w-[19rem] max-sm:text-left'>
-          {ProjectDesc}
-          </p>
-          <div className='flex gap-3'>
-          <img className='w-[40px] h-[32px] mb-20 mt-14 max-sm:mb-8' src={Tailwind} alt='Tailwind-logo' />
+    <div
+      data-aos="fade-right"
+      data-aos-duration="1500"
+      className="bg-neutral-900 rounded-3xl ring-1 ring-neutral-500 w-1200 h-620 my-10"
+    >
+      <div className="py-16 px-16 max-[480px]:py-8 max-[480px]:px-8">
+        <div className="grid grid-cols-3 h-full max-[480px]:flex max-[480px]:flex-col">
+          {/* Text div */}
+          <div className="pr-4  flex flex-col">
+            <div className="h-full items-start col-span-1 flex flex-col justify-around max-sm:mb-6">
+              <h1 className="mb-6 font-Inter font-bold text-4xl text-left max-sm:w-[16rem] max-sm:text-left">
+                {ProjectName}
+              </h1>
+              <span className="py-6 font-Inter font-normal text-base text-left max-sm:text-left max-sm:w-[15rem]">
+                {Projectstack}
+              </span>
+              <p className="font-Inter font-normal text-lg text-left max-sm:w-[19rem] max-sm:text-left">
+                {ProjectDesc}
+              </p>
+              <div className="flex gap-3">
+                {/* <img className='w-[40px] h-[32px] mb-20 mt-14 max-sm:mb-8' src={tailwindlogo} alt='tailwindlogo-logo' />
           <img className='w-[40px] h-[32px] mb-20 mt-14 max-sm:mb-8' src={rctlogo} alt='React-logo' />
-          <img className='w-[40px] h-[32px] mb-20 mt-14 max-sm:mb-8' src={figmalogo} alt='Figma-logo' />
+          <img className='w-[40px] h-[32px] mb-20 mt-14 max-sm:mb-8' src={figmalogo} alt='Figma-logo' /> */}
+                {logos.map((logo, index) => (
+                  <img
+                    key={index}
+                    className="w-[40px] h-[40px] mb-20 mt-14 max-sm:mb-8"
+                    src={logo}
+                    alt={"Logo not loading for " + logo}
+                  />
+                ))}
+              </div>
+              <a href={projects} target="blank">
+                <button className="ring-2 ring-neutral-300 rounded-lg py-3 px-6 font-Inter font-semibold text-base">
+                  View Project
+                </button>
+              </a>
+            </div>
           </div>
-          <a href={projects} target='blank'>
-          <button className='ring-2 ring-neutral-300 rounded-lg py-3 px-6 font-Inter font-semibold text-base'>
-           View Project
-          </button>
-          </a>
-        </div>
-        </div>
 
-        {/* Image div */}
-        <div className='col-span-2'>
-          <img src={src} alt='cashflow' className='w-full h-full object-cover' />
+          {/* Image div */}
+          <div className="col-span-2">
+            <img
+              src={src}
+              alt="cashflow"
+              className="w-full h-full object-cover"
+            />
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
--- Frontend/builders_landing/src/components/OurBuilds.jsx
+++ Frontend/builders_landing/src/components/OurBuilds.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
-import Card from './Card'
+import React from 'react';
+import Card from './Card';
 import buildersHub from '../assets/buildersHub.png';
 import yogawebsite from '../assets/yogaproject.png';
 import cashflow from '../assets/cashflow.png';
 import { Link } from 'react-router-dom';
+import tailwindlogo from '../assets/Tailwind.png';
+import rctlogo from '../assets/React-logo.png';
+import figmalogo from '../assets/Figma-logo.png';
+import nodejslogo from '../assets/nodejs-logo.png';
+import expresslogo from '../assets/express-logo.png';
+import mongodblogo from '../assets/mongodb-logo.png';
+import webflowlogo from '../assets/webflow-logo.png';
+import sanitylogo from '../assets/sanity-logo.png';
+
+// Define an object to map skill names to their respective logos
+const skillLogos = {
+  tailwind: tailwindlogo,
+  react: rctlogo,
+  figma: figmalogo,
+  nodejs: nodejslogo,
+  express: expresslogo,
+  mongodb: mongodblogo,
+  webflow: webflowlogo,
+  sanity: sanitylogo
+};
+
+// Define an array of skills for each project
+const projectSkills = {
+  buildersHub: ['tailwind', 'react', 'figma'],
+  yogawebsite: ['react', 'sanity', 'figma'],
+  cashflow: ['react', 'tailwind', 'express', 'mongodb','webflow'],
+};
 
 const OurBuilds = () => {
   return (
     <div data-aos="fade-up" data-aos-duration="1500" id='our-builds-section' className='mb-48 max-[480px]:mb-24 max-[480px]:p-4  max-[480px]:flex max-[480px]:flex-col'>
-        <h1 className="text-left mb-5 font-Inter font-bold text-5xl max-[480px]:w-[12rem] max-[480px]:text-4xl max-[480px]:text-center"> Our Builds</h1>
-        <p className=" w-[48rem] text-left mb-10 font-Inter font-normal text-lg text-neutral-400 max-[480px]:w-[21rem]">Here's a glimpse of some of my recent and exciting projects.
-         Each of them reflects my focus on user-centered design and my commitment to excellence in user experience.</p>
-         <Card src={buildersHub} projects="https://buildershub.vercel.app/" ProjectName="Builders Hub" Projectstack="React + Tailwind + Figma - Frontend" ProjectDesc="Crafted Builders Hub with React and Tailwind, designed in Figma. Our aim? Enhance user experience and foster community collaboration for building projects. Connect with fellow builders to accelerate your project's development and showcase it on Builders Hub!"/>
-        <Card src={yogawebsite} projects="https://yoga-website-jet.vercel.app/" ProjectName="Art of Learning Institute" Projectstack="React + Sanity + Figma - Fullstack" ProjectDesc="We upgraded up the Art of Learning Institute's yoga business with a polished UI design, using React and CSS for the frontend. The Builders created the UI in Figma and conducted design research to make sure it's user-friendly. The outcome? A smooth interface that boosts the brand and keeps users engaged."/>
-        <Card src={cashflow} projects="https://cashflow-dd61aa.design.webflow.com/" ProjectName="Cashflow" Projectstack="React + TailwinD + Express + MongoDB + Figma - Fullstack" ProjectDesc=" Developed Cashflow, a payment application enabling users to securely log in, authenticate, and transfer funds seamlessly. Leveraging React for the frontend and Express with MongoDB for the backend, we ensured robust functionality. Additionally, integrated a Text-To-Speech module to enhance user experience."/>
-        <Link to="/projects" className='font-Inter font-semibold text-base underline'>View all projects</Link>    
-        </div>
+      <h1 className="text-left mb-5 font-Inter font-bold text-5xl max-[480px]:w-[12rem] max-[480px]:text-4xl max-[480px]:text-center"> Our Builds</h1>
+      <p className=" w-[48rem] text-left mb-10 font-Inter font-normal text-lg text-neutral-400 max-[480px]:w-[21rem]">Here's a glimpse of some of my recent and exciting projects. Each of them reflects my focus on user-centered design and my commitment to excellence in user experience.</p>
+      <Card logos={projectSkills.buildersHub.map(skill => skillLogos[skill])} src={buildersHub} projects="https://buildershub.vercel.app/" ProjectName="Builders Hub" Projectstack="React + Tailwind + Figma - Frontend" ProjectDesc="Crafted Builders Hub with React and Tailwind, designed in Figma. Our aim? Enhance user experience and foster community collaboration for building projects. Connect with fellow builders to accelerate your project's development and showcase it on Builders Hub!"/>
+      <Card logos={projectSkills.yogawebsite.map(skill => skillLogos[skill])} src={yogawebsite} projects="https://yoga-website-jet.vercel.app/" ProjectName="Art of Learning Institute" Projectstack="React + Sanity + Figma - Fullstack" ProjectDesc="We upgraded up the Art of Learning Institute's yoga business with a polished UI design, using React and CSS for the frontend. The Builders created the UI in Figma and conducted design research to make sure it's user-friendly. The outcome? A smooth interface that boosts the brand and keeps users engaged."/>
+      <Card logos={projectSkills.cashflow.map(skill => skillLogos[skill])} src={cashflow} projects="https://cashflow-dd61aa.design.webflow.com/" ProjectName="Cashflow" Projectstack="React + TailwinD + Express + MongoDB + Figma - Fullstack" ProjectDesc=" Developed Cashflow, a payment application enabling users to securely log in, authenticate, and transfer funds seamlessly. Leveraging React for the frontend and Express with MongoDB for the backend, we ensured robust functionality. Additionally, integrated a Text-To-Speech module to enhance user experience."/>
+      <Link to="/projects" className='font-Inter font-semibold text-base underline'>View all projects</Link>    
+    </div>
   )
 }
 
-export default OurBuilds
+export default OurBuilds;
