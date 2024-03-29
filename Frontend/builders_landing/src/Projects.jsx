0d6ca31fa91e53e9d354139ce75a83608681373a
hardikk kamboj <kambojpushkal@gmail.com>
2024-03-29 21:51:07 +0530

    added Navbar in projects section

--- Frontend/builders_landing/src/Projects.jsx
+++ Frontend/builders_landing/src/Projects.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 import { Link } from 'react-router-dom';
 const Projects = () => {
   return (
+
     <div class="lg:px-24 lg:py-24 md:max-lg:py-20 md:max-lg:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:max-lg:gap-28 gap-16">
+                        <Navbar />
             <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
                 <div class="relative">
                     <div class="absolute">
--- Frontend/builders_landing/src/components/Navbar.jsx
+++ Frontend/builders_landing/src/components/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { Link } from 'react-scroll';
+import React, { useState } from 'react';
+import { Link as ScrollLink } from 'react-scroll'; // Renamed alias for react-scroll Link
+import { Link as RouterLink } from 'react-router-dom'; // Renamed alias for react-router-dom Link
 
 const Navbar = () => {
   return (
     <div className='w-full flex pt-6 absolute inset-0 z-[1]'>
       <div className='text-base font-semibold items-center flex-1 font-Inter'>
-        <Link
+        {/* <RouterLink
+          to='/'
+          className='mx-6 py-2 my-2 bg-zinc-900 px-3.5 rounded-lg border-stone-500 ring-1 ring-gray-400 hover:bg-zinc-100 hover:text-black hover:cursor-pointer'
+        >
+          Home
+        </RouterLink> */}
+        <ScrollLink
           to='our-builds-section'
           spy={true}
           smooth={true}
@@ -14,8 +21,8 @@ const Navbar = () => {
           className='mx-6 py-2 my-2 bg-zinc-900 px-3.5 rounded-lg border-stone-500 ring-1 ring-gray-400 hover:bg-zinc-100 hover:text-black hover:cursor-pointer'
         >
           Work
-        </Link>
-        <Link
+        </ScrollLink>
+        <ScrollLink
           to='about-section'
           spy={true}
           smooth={true}
@@ -24,8 +31,8 @@ const Navbar = () => {
           className='mx-6 hover:cursor-pointer'
         >
           About
-        </Link>
-        <Link
+        </ScrollLink>
+        <ScrollLink
           to='contact-section'
           spy={true}
           smooth={true}
@@ -34,7 +41,13 @@ const Navbar = () => {
           className='mx-6 bg-gradient-to-r from-amber-300 via-cyan-400 to-indigo-400 inline-block text-transparent bg-clip-text hover:cursor-pointer'
         >
           Contact
-        </Link>
+        </ScrollLink>
+        {/* <RouterLink
+          to='/projects'
+          className='mx-6 py-2 my-2 bg-zinc-900 px-3.5 rounded-lg border-stone-500 ring-1 ring-gray-400 hover:bg-zinc-100 hover:text-black hover:cursor-pointer'
+        >
+          Projects
+        </RouterLink> */}
       </div>
     </div>
   );
