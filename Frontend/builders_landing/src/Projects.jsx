59bbc76ba5e69d5f994fb17a7dfc587e12992c59
hardikk kamboj <kambojpushkal@gmail.com>
2024-03-30 03:47:13 +0530

    Fix UI layout and enable Preloader

--- Frontend/builders_landing/src/Projects.jsx
+++ Frontend/builders_landing/src/Projects.jsx
@@ -4,25 +4,48 @@ import { Link } from 'react-router-dom';
 const Projects = () => {
   return (
 
-    <div class="lg:px-24 lg:py-24 md:max-lg:py-20 md:max-lg:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:max-lg:gap-28 gap-16">
-                        <Navbar />
-            <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
-                <div class="relative">
-                    <div class="absolute">
-                        <div class="">
-                            <h1 class="my-2 text-white font-bold text-2xl">
-                                Hi we are currently Building this Page!
-                                Stay Tuned!
-                            </h1>
-
+    <div className="lg:px-24 lg:py-24 md:max-lg:py-20 md:max-lg:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:max-lg:gap-28 gap-16">
+            {/* <Navbar /> */}
+        <section className="flex flex-col items-center">
+            {/* Main div to enclose the 2 divs */}
+            <div className="relative mb-16 w-full items-start gap-6 lg:flex xl:gap-8 mx-auto">
+                    {/* Div for sticky navbar for filters */}
+                        <div className="w-full lg:sticky lg:left-0 lg:top-20 lg:w-48 lg:flex-shrink-0">
+                    <div className="relative w-full rounded-md group border-gray-800 bg-gray-800 text-gray-300 placeholder-gray-300 shadow-lg mb-5 sm:max-w-xs">
+                        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 opacity-50 transition-opacity group-focus-within:opacity-100 group-hover:opacity-100">
+                        <div className="h-5 w-5 text-white/30">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon">
+                            <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd"></path>
+                            </svg>
                         </div>
+                        </div>
+                        <input className="block w-full rounded-md border border-transparent py-2 pl-10 pr-3 text-sm placeholder-opacity-60 outline-none transition hover:placeholder-opacity-100 focus:border-blue-500 focus:placeholder-opacity-30 focus:ring-4 focus:ring-blue-500/30 bg-gray-300 bg-opacity-5 text-gray-300 placeholder-gray-300 ring-offset-gray-900 hover:bg-opacity-10 focus:bg-gray-800 focus:bg-opacity-100" placeholder="Search…" />
                     </div>
+                    <nav aria-label="Sidebar" className="space-y-2 z-10 hidden lg:block">
+                        <a aria-current="page" className="group flex items-center rounded-3xl px-3 py-2 text-sm font-medium ring-1 cursor-default bg-yellow-400 bg-opacity-30 text-white ring-yellow-400 ring-opacity-80">
+                        <span className="truncate">All</span>
+                        </a>
+                        <a aria-current="false" className="group flex items-center rounded-3xl px-3 py-2 text-sm font-medium ring-1 ease-out-back-smooth cursor-pointer text-white/80 ring-white/20 transition duration-200 hover:scale-105 hover:bg-yellow-400 hover:bg-opacity-20 hover:text-white/90 hover:ring-yellow-400 hover:ring-opacity-50 active:scale-95">
+                        <span className="truncate">Frontend</span>
+                        </a>
+                        <a aria-current="false" className="group flex items-center rounded-3xl px-3 py-2 text-sm font-medium ring-1 ease-out-back-smooth cursor-pointer text-white/80 ring-white/20 transition duration-200 hover:scale-105 hover:bg-yellow-400 hover:bg-opacity-20 hover:text-white/90 hover:ring-yellow-400 hover:ring-opacity-50 active:scale-95">
+                        <span className="truncate">Backend</span>
+                        </a>
+                        <a aria-current="false" className="group flex items-center rounded-3xl px-3 py-2 text-sm font-medium ring-1 ease-out-back-smooth cursor-pointer text-white/80 ring-white/20 transition duration-200 hover:scale-105 hover:bg-yellow-400 hover:bg-opacity-20 hover:text-white/90 hover:ring-yellow-400 hover:ring-opacity-50 active:scale-95">
+                        <span className="truncate">Fullstack</span>
+                        </a>
+                    </nav>
+                    </div>
+                    
+                    {/* Div for grid */}
 
-                </div>
-            </div>
-            <div>
-                <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
-            </div>
+            <div className="grid w-full grid-cols-2 grid-rows-[9rem] gap-2 md:grid-cols-3 xl:grid-cols-3 xl:gap-3 2xl:grid-cols-4">
+            <div className="lg:hover:preserve-3d group relative flex h-36 flex-col items-center rounded-lg text-center text-white outline-none ring-offset-gray-900 hover:z-10 focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-4">
+      {/* Your onClick handler here */}
+    </div>
+            </div>  
+        </div>
+        </section>
         </div>
   )
 }
